12d1d405cad0e6176e4a5a3106c0a8e391d1f082
Yury <kotovyuriy27@gmail.com>
2019-07-07 19:06:10 +0300

    added link, input, button styles

--- desktop.bundles/experiment/experiment.bemjson.js
+++ desktop.bundles/experiment/experiment.bemjson.js
@@ -3,8 +3,9 @@ module.exports = {
     title: 'Title of the experiment page',
     favicon: '/favicon.ico',
     head: [
-        { elem: 'meta', attrs: { name: 'description', content: '' }},
-        { elem: 'css', url: 'experiment.min.css' }
+        { elem : 'meta', attrs : { name : 'description', content : '' } },
+        { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
+        { elem : 'css', url : 'experiment.min.css' }
     ],
     scripts: [{ elem: 'js', url: 'experiment.min.js' }],
     content: [
@@ -13,13 +14,46 @@ module.exports = {
             content: {
                 block: 'layout',
                 content: [
-                    {
-                        elem: 'left',
-                        content: 'left here'
-                    },
+                {
+                    elem: 'left',
+                    content: {
+                        tag: 'form',
+                        attrs: { action: 'https://yandex.ru/yandsearch' },
+                        content: [
+                            {
+                                block: 'input',
+                                name: 'text',
+                                val: 'Find'
+                            },
+                            {
+                                block: 'button',
+                                mods: { type: 'submit' },
+                                content: 'Search'
+                            }
+                        ]
+                    }
+                },
                     {
                         elem: 'right',
-                        content: 'right here'
+                        content: {
+                            block: 'logo',
+                            content: [
+                                {
+                                    block: 'link',
+                                    url: 'https://ru.bem.info',
+                                    content: [
+                                        {
+                                            block: 'image',
+                                            url: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png'
+                                        },
+                                        {
+                                            elem: 'slogan',
+                                            content: 'A new way of thinking'
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
                     }
                 ]
             }
