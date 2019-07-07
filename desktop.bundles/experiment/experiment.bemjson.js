8a73490299eb6c957a8ba311ab4771bf21d59128
Yury <kotovyuriy27@gmail.com>
2019-07-07 18:17:54 +0300

    added experiment page bundle

---
+++ desktop.bundles/experiment/experiment.bemjson.js
@@ -0,0 +1,13 @@
+module.exports = {
+    block: 'page',
+    title: 'Title of the experiment page',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'css', url: 'index.min.css' }
+    ],
+    scripts: [{ elem: 'js', url: 'index.min.js' }],
+    content: [
+        { block : 'head' }
+    ]
+};
