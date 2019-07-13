ae3cf62d9e7b4e6f9d69f4298fdb92170f0acae4
Yury <kotovyuriy27@gmail.com>
2019-07-14 01:17:03 +0300

    created goods block

--- desktop.bundles/experiment/experiment.bemjson.js
+++ desktop.bundles/experiment/experiment.bemjson.js
@@ -57,6 +57,42 @@ module.exports = {
                     }
                 ]
             }
+        },
+        {
+            block: 'goods',
+            goods: [
+                {
+                    title: 'iPhone 7 128Gb',
+                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__iphone7.png',
+                    price: '47 000',
+                    url: '/'
+                },
+                {
+                    title: 'Samsung Galaxy A7 32Gb',
+                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__samsung.png',
+                    price: '26 000',
+                    url: '/'
+                },
+                {
+                    title: 'Meizu MX5 16Gb',
+                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__meizu.png',
+                    price: '15 000',
+                    url: '/'
+                },
+                {
+                    title: 'iPhone 6s 32GB',
+                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__iphone6s.png',
+                    price: '***',
+                    url: '/',
+                    new: true
+                },
+                {
+                    title: 'Xiaomi Redmi 4X 16Gb',
+                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__xiaomi.png',
+                    price: '10 000',
+                    url: '/'
+                }
+            ]
         }
     ]
 };
