7830c74a346f9b8b33fe6c8e0110769e888595b0
Yury <kotovyuriy27@gmail.com>
2019-07-24 18:08:27 +0300

    added box block added mixin

--- .enb/make.js
+++ .enb/make.js
@@ -39,6 +39,7 @@ const techs = {
         { path: 'node_modules/bem-components/desktop.blocks', check: false },
         { path: 'node_modules/bem-components/design/common.blocks', check: false },
         { path: 'node_modules/bem-components/design/desktop.blocks', check: false },
+        { path: 'node_modules/j/blocks', check: false },
         'common.blocks',
         'desktop.blocks'
     ];
--- desktop.bundles/experiment/experiment.bemjson.js
+++ desktop.bundles/experiment/experiment.bemjson.js
@@ -1,94 +1,100 @@
 module.exports = {
     block: 'page',
-    title: 'Title of the experiment page',
+    title: 'Title of the page',
     favicon: '/favicon.ico',
     head: [
-        { elem : 'meta', attrs : { name : 'description', content : '' } },
-        { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
-        { elem : 'css', url : 'experiment.min.css' }
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'css', url: 'experiment.min.css' }
     ],
     scripts: [{ elem: 'js', url: 'experiment.min.js' }],
     content: [
-        { 
-            block : 'head',
-            content: {
-                block: 'layout',
-                content: [
+        {
+            block: 'head',
+            mix: { block: 'box', js: true },
+            content: [
                 {
-                    elem: 'left',
-                    content: {
-                        tag: 'form',
-                        attrs: { action: 'https://yandex.ru/yandsearch' },
-                        content: [
-                            {
-                                block: 'input',
-                                name: 'text',
-                                val: 'Find'
-                            },
-                            {
-                                block: 'button',
-                                mods: { type: 'submit' },
-                                content: 'Search'
+                    block: 'layout',
+                    content: [
+                        {
+                            elem: 'left',
+                            content: {
+                                tag: 'form',
+                                attrs: { action: 'http://yandex.ru/yandsearch' },
+                                content: [
+                                    {
+                                        block: 'input',
+                                        name: 'text',
+                                        val: 'Find'
+                                    },
+                                    {
+                                        block: 'button',
+                                        mods: { type: 'submit' },
+                                        content: 'Search'
+                                    }
+                                ]
                             }
-                        ]
-                    }
-                },
-                    {
-                        elem: 'right',
-                        content: {
-                            block: 'logo',
-                            content: [
-                                {
-                                    block: 'link',
-                                    url: 'https://ru.bem.info',
-                                    content: [
-                                        {
-                                            block: 'image',
-                                            url: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png'
-                                        },
-                                        {
-                                            elem: 'slogan',
-                                            content: 'A new way of thinking'
-                                        }
-                                    ]
-                                }
-                            ]
+                        },
+                        {
+                            elem: 'right',
+                            content: {
+                                block: 'logo',
+                                content: [
+                                    {
+                                        block: 'link',
+                                        url: 'http://ru.bem.info',
+                                        content: [
+                                            {
+                                                block: 'image',
+                                                url: 'http://varya.me/online-shop-dummy/desktop.blocks/b-logo/b-logo.png'
+                                            },
+                                            {
+                                                elem: 'slogan',
+                                                content: 'A new way of thinking'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                },
+                {
+                    block: 'box',
+                    elem: 'switcher'
+                }
+            ]
         },
         {
             block: 'goods',
             goods: [
                 {
                     title: 'iPhone 7 128Gb',
-                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__iphone7.png',
+                    image: 'https://cdn.rawgit.com/bem-site/bem-method/bem-project/articles/start-with-project-stub/iphone7.png',
                     price: '47 000',
                     url: '/'
                 },
                 {
                     title: 'Samsung Galaxy A7 32Gb',
-                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__samsung.png',
+                    image: 'https://cdn.rawgit.com/bem-site/bem-method/bem-project/articles/start-with-project-stub/samsung.png',
                     price: '26 000',
                     url: '/'
                 },
                 {
                     title: 'Meizu MX5 16Gb',
-                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__meizu.png',
+                    image: 'https://cdn.rawgit.com/bem-site/bem-method/bem-project/articles/start-with-project-stub/meizu.png',
                     price: '15 000',
                     url: '/'
                 },
                 {
                     title: 'iPhone 6s 32GB',
-                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__iphone6s.png',
+                    image: 'https://cdn.rawgit.com/bem-site/bem-method/bem-project/articles/start-with-project-stub/iphone6s.png',
                     price: '***',
                     url: '/',
                     new: true
                 },
                 {
                     title: 'Xiaomi Redmi 4X 16Gb',
-                    image: 'https://github.com/bem-site/bem-method/blob/bem-info-data/platform/tutorials/start-with-project-stub/start-with-project-stub__xiaomi.png',
+                    image: 'https://cdn.rawgit.com/bem-site/bem-method/bem-project/articles/start-with-project-stub/xiaomi.png',
                     price: '10 000',
                     url: '/'
                 }
